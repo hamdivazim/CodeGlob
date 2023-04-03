62104d7bb7263bc20ba797ac42860aa3f8028def
hamdivazim <coding.wizard4@gmail.com>
2023-04-03 11:38:41 +0100

    Add initial home page

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,27 @@
-function redirect_from_index() {
-    window.location.href = "frontend/home.html";
+function redirect(destination) {
+    window.location.href = destination;
+}
+
+function showNav() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    document.getElementById("logo-nav").src = "../assets/logo-transparent.png";
+  } else {
+    x.className = "topnav";
+    document.getElementById("logo-nav").src = "../assets/transparent-logo-small.png";
+  }
+}
+
+function searchNewPageQueries() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  const type = urlParams.get('type')
+
+  if (type == "post") {
+    document.getElementById("newsnippet-content").className = "hidden";
+  } else if (type == "snippet") {
+    document.getElementById("newpost-content").className = "hidden";
+  }
 }
